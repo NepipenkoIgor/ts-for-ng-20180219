9dd1d0ba42e4bec226186e87c6340cfd2a03cdbc
svetlanasharipova <sharipova_sveta@mail.ru>
2018-03-02 15:06:46 +0300

    fix homework1

---
+++ homework/svetlana-sharipova/homework1.spec.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import { getUnique, isInArray, reverseWords, summator } from './homework1';
+
+describe('reverseWords', () => {
+    it('should reverse "s1Tar3t 2 hellow" -> "t1raT3s 2 wolleh"', () => {
+        const result = reverseWords('s1Tar3t 2 hellow');
+        expect(result)
+            .to
+            .equal('t1raT3s 2 wolleh');
+    });
+});
+
+describe('reverseWords', () => {
+    it('should reverse "s1ta$%r3t 2 hel^loW" -> "t1ra$%t3s 2 Wol^leh"', () => {
+        const result = reverseWords('s1ta$%r3t 2 hel^loW');
+        expect(result)
+            .to
+            .equal('t1ra$%t3s 2 Wol^leh');
+    });
+});
+
+describe('isInArray', () => {
+    it('should return true isInArray([1,"aaa", 2,3, "ww"],3,4,"aaa")', () => {
+        const result = isInArray([1, 'aaa', 2, 3, 4, 'ww'], 3, 4, 'aaa');
+        expect(result)
+            .to
+            .equal(true);
+    });
+});
+
+describe('summator', () => {
+    it('should return 27 summator(10, "qw", "-22", "6", 25, 8)', () => {
+        const result = summator([10, 'qw', '-22', '6', 25, 8]);
+        expect(result)
+            .to
+            .equal(27);
+    });
+});
+
+describe('getUnique', () => {
+    it('should return [10, "qw", "-22", 8] of [10, "qw", "-22", 8, "qw", 10, "-22", 8] ', () => {
+        const result = getUnique(10, 'qw', '-22', 8, 'qw', 10, '-22', 8);
+        const ar: (string | number)[] = [10, 'qw', '-22', 8];
+        expect(result)
+            .to.deep
+            .equal(ar);
+    });
+});
--- homework/svetlana-sharipova/homework1.ts
+++ homework/svetlana-sharipova/homework1.ts
@@ -1,81 +1,62 @@
-//1
-function isInArray(a: any[], ...args: any[]) {
-  if (args.length == 0) {
+// 1
+export function isInArray(a: any[], ...args: any[]) {
+  if (args.length === 0) {
     return false;
   }
-
   return args.every((arg) => {
     return a.indexOf(arg) >= 0;
   });
 }
 
-
-//2
-function summator(...args: (number | string)[]) {
-
-  var r: number = 0;
-
-  for (let a of args) {
-    typeof a === 'string'
-      ? r += parseToInt(a)
-      : r += a;
-  }
-  return r;
-
+// 2
+export function summator(arr: (number | string)[]) {
+   return arr.reduce((a, b) => parseToInt(a) + parseToInt(b));
 }
 
-function parseToInt(a: string) {
-
-  var r = parseInt(a);
-  return Number.isNaN(r)
-    ? 0
-    : r;
-
+function parseToInt(a: string | number): number {
+  if (typeof a === 'string') {
+      const r: number = parseInt(a, 10);
+      return isNaN(r) ? 0 : r;
+  } else {return  a; }
 }
 
-//3
-function getUnique(...args: any[]) {
-  var res: any[] = [];
+// 3
+export function getUnique(...args: (string | number)[]) {
+  const res: (string | number)[] = [];
 
-  for (let a of args) {
-    if (res.indexOf(a) == -1) {
-      res.push(a);
-    }
+  for (const a of args) {
+    if (!res.includes(a)) {
+      res.push(a); }
   }
 
   return res;
-
 }
 
-//4
-function reverseWords(input: string): string {
-  let res: string = "";
+// 4
+export function reverseWords(input: string): string {
+  let res: string = '';
 
-  let input_split: string[] = input.split(' ');
+  const inputSplit: string[] = input.split(' ');
 
-  for (let s in input_split) {
-    let charArray = s.split('');
+  for (const s of inputSplit) {
+    const charArray = s.split('');
 
-    //перевернутая строка без символов
-    let nosymbols = charArray.filter(c => isLetter(c))
-      .reverse();
+    // перевернутая строка без символов
+    const nosymbols = charArray.filter((c) => isLetter(c)).reverse();
 
-    let tmp = nosymbols.join("");
+    let tmp = nosymbols.join('');
     for (let i = 0; i < charArray.length; i++) {
       if (!isLetter(charArray[i])) {
         tmp = [tmp.slice(0, i), charArray[i], tmp.slice(i)].join('');
       }
     }
 
-    res += tmp + " ";
+    res += `${tmp} `;
   }
-
   return res.trim();
 }
 
 function isLetter(str: string) {
+  // case-insensitive
   return str.length === 1 && str.match(/[a-z]/i);
 }
-
-
-console.log(reverseWords("s1tar3t 2 hellow"));
