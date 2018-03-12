51a0e7be74731fe0fc287fca168cc8c524d940ea
Шишминцев Григорий Владимирович <GVSHISHM@mts.ru>
2018-03-12 11:33:06 +0300

    summator tests

--- homework/grigoriy_shishmintsev/summator/summator.ts
+++ homework/grigoriy_shishmintsev/summator/summator.ts
@@ -1,16 +1,18 @@
-  // 2)
-  //  писать функцию summator(), которая сумирует переданые ей аргументы.
-  //  Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
+// 2)
+//  писать функцию summator(), которая сумирует переданые ей аргументы.
+//  Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
 
-  export const summator = (...args: (number | string)[]): number => {
+type StrNum = string | number;
 
-    const parsed: number[] = args.map(item => {
-      if (typeof item === 'string') {
-        return parseInt(item);
-      }
-      return item;
+export const summator = (...args: StrNum[]): number => {
 
-    });
+	const parsed: number[] = args.map((item) => {
+		if (typeof item === 'string') {
+			return parseInt(item, 10);
+		}
+		return item;
 
-    return parsed.reduce((acc, curr) => acc += curr, 0);
-  }
+	});
+
+	return parsed.reduce((acc, curr) => acc += curr, 0);
+};
